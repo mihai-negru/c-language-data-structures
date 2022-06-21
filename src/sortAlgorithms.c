95e059a45c7b652bcc59f38953da4a8cf5250dad
Mihai Negru <determinant289@gmail.com>
2022-06-21 16:00:51 +0300

    Add copyright to every file

--- src/sortAlgorithms.c
+++ src/sortAlgorithms.c
@@ -1,1 +1,25 @@
+/**
+ * @file sortAlgorithms.c
+ * @author Mihai Negru (determinant289@gmail.com)
+ * @version 1.0.0
+ * @date 2022-06-21
+ * 
+ * @copyright Copyright (C) 2022-2023 Mihai Negru <determinant289@gmail.com>
+ * This file is part of C-language-Data-Structures.
+ *
+ * C-language-Data-Structures is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * C-language-Data-Structures is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Collections-C.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 #include "./include/sortAlgorithms.h"
