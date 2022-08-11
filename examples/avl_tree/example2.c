d6d12b7e5571ad95a2259daa357184603a683577
Mihai Negru <determinant289@gmail.com>
2022-08-11 21:03:24 +0300

    Finished avl tree, example 3 is benchmark

--- examples/avl_tree/example2.c
+++ examples/avl_tree/example2.c
@@ -1,187 +1,159 @@
 #include <scl_datastruc.h>
 #include <time.h>
 
+#define MAX_NAME 20
+#define MAX_SURNAME 30
+
+typedef struct student_s {
+    char *name;
+    char *surname;
+    unsigned int age;
+    unsigned int class;
+    unsigned char class_label;
+    double GPA;
+    int index;
+} student_t;
+
+int32_t compare_student(const void * const elem1, const void * const elem2) {
+    const student_t * const s1 = elem1;
+    const student_t * const s2 = elem2;
+
+    int cmp = strncmp(s1->name, s2->name, MAX_NAME);
+
+    if (0 != cmp) {
+        return cmp;
+    }
+
+    cmp = strncmp(s1->surname, s2->surname, MAX_SURNAME);
+
+    if (0 != cmp) {
+        return cmp;
+    }
+
+    if (s1->GPA > s2->GPA) {
+        cmp = 1;
+    } else if (s1->GPA < s2->GPA) {
+        cmp = -1;
+    } else {
+        cmp = 0;
+    }
+
+    if (cmp == 0) {
+        return s1->index - s2->index;
+    }
+
+    return cmp;
+}
+
+void free_student(void * const elem) {
+    if (NULL != elem) {
+        student_t * const st = elem;
+
+        free(st->name);
+        st->name = NULL;
+
+        free(st->surname);
+        st->surname = NULL;
+
+        st->age = 0;
+        st->class = 0;
+        st->class_label = '\0';
+        st->GPA = 0.0;
+        st->index = -1;
+    }
+}
+
+void print_student(void * const elem) {
+    if (NULL != elem) {
+        const student_t * const st = elem;
+
+        printf("Student no. %d\n", st->index);
+        printf("NAME: %s\n", st->name);
+        printf("SURNAME: %s\n", st->surname);
+        printf("Student GPA: %lf\n", st->GPA);
+        printf("Student age: %u\n\n", st->age);
+    }
+}
+
+void two_years_passed(void * const elem) {
+    if (NULL != elem) {
+        student_t * const st = elem;
+
+        st->age = st->age + 2;
+
+        print_student(st);
+    }
+}
+
 int main(void) {
+    FILE *fin = NULL;
+
+    if ((fin = freopen("example2.in", "r", stdin)) == NULL) {
+        exit(EXIT_FAILURE);
+    }
+
+    avl_tree_t *data_base = create_avl(&compare_student, &free_student, sizeof(student_t));
+
+    if (NULL == data_base) {
+        exit(EXIT_FAILURE);
+    }
+
+    scl_error_t err = SCL_OK;
+
+
+    int number_of_students = 0;
+    int read = scanf("%d", &number_of_students);
+
+    if (read > 1) {
+        exit(EXIT_FAILURE);
+    }
+
+    student_t st; // Encapsulate the reading
+
+    for (int i = 0; i < number_of_students; ++i) {
+        st.name = malloc(MAX_NAME);
+        st.surname = malloc(MAX_SURNAME);
+
+        read = scanf("%s %s", st.name, st.surname);
+
+        if (read > 2) {
+            exit(EXIT_FAILURE);
+        }
+
+        read = scanf("%u %u %c %lf", &st.age, &st.class, &st.class_label, &st.GPA);
+
+        if (read > 4) {
+            exit(EXIT_FAILURE);
+        }
+
+        st.index = i;
+
+        err = avl_insert(data_base, toptr(st));
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    fclose(fin);
+
     FILE *fout = NULL;
 
     if ((fout = freopen("out_2.txt", "w", stdout)) == NULL) {
         exit(EXIT_FAILURE);
     }
 
-    printf("In this example we will try to do different operations on AVL Tree and measure the time\n");
+    printf("In the data base are %lu students\n\n", get_avl_size(data_base));
 
-    /* Init the working strucutres */
-    double exec_time = 0.0;
+    avl_traverse_inorder(data_base, &print_student);
+    printf("\n\n");
 
-    scl_error_t err = SCL_OK;
+    printf("After two years our students got older so let's change their age\n\n");
+    avl_traverse_inorder(data_base, &two_years_passed);    
 
-    avl_tree_t *my_tree = create_avl(&compare_int, NULL, sizeof(int));
-    /* End to init the working structures */
-
-    /* Insert 100 ints into the avl and count the time */
-    clock_t begin = clock();
-
-    for (int i = 0; i < 100; ++i) {
-        err = avl_insert(my_tree, toptr(i));
-
-        if (SCL_OK != err) {
-            scl_error_message(err);
-        }
-    }
-
-    clock_t end = clock();
-
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Inserting 100 ints into avl: %lf sec\n", exec_time);
-    /* End to insert 100 ints into the avl */
-
-    /* Delete 100 roots from avl and count the time */
-    begin = clock();
-
-    for (int i = 0; i < 100; ++i) {
-        err = avl_delete(my_tree, my_tree->root->data);
-
-        if (SCL_OK != err) {
-            scl_error_message(err);
-        }
-    }
-
-    end = clock();
-
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Deleting 100 ints roots from avl: %lf sec\n", exec_time);
-    /* End to delete 100 roots from avl */
-
-    /* Free avl tree with size of 100 and count time */
-    for (int i = 0; i < 100; ++i) {
-        avl_insert(my_tree, toptr(i));
-    }
-
-    begin = clock();
-
-    free_avl(my_tree);
-    
-    end = clock();
-    
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Freeing avl with 100 ints: %lf sec\n\n", exec_time);
-    
-    my_tree = NULL;
-    /* End to free avl tree with size of 100 */
-
-    ////////////////////////////////////////////////////////////// Increase size 1
-
-    my_tree = create_avl(&compare_int, NULL, sizeof(int));
-
-    /* Insert 100000 ints into the avl and count the time */
-    begin = clock();
-
-    for (int i = 0; i < 100000; ++i) {
-        err = avl_insert(my_tree, toptr(i));
-
-        if (SCL_OK != err) {
-            scl_error_message(err);
-        }
-    }
-
-    end = clock();
-
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Inserting 100000 ints into avl: %lf sec\n", exec_time);
-    /* End to insert 100000 ints into the avl */
-
-    /* Delete 100000 roots from avl and count the time */
-    begin = clock();
-
-    for (int i = 0; i < 100000; ++i) {
-        err = avl_delete(my_tree, my_tree->root->data);
-
-        if (SCL_OK != err) {
-            scl_error_message(err);
-        }
-    }
-
-    end = clock();
-
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Deleting 100000 ints roots from avl: %lf sec\n", exec_time);
-    /* End to delete 100000 roots from avl */
-
-    /* Free avl tree with size of 100000 and count time */
-    for (int i = 0; i < 100000; ++i) {
-        avl_insert(my_tree, toptr(i));
-    }
-
-    begin = clock();
-
-    free_avl(my_tree);
-    
-    end = clock();
-    
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Freeing avl with 100000 ints: %lf sec\n\n", exec_time);
-    
-    my_tree = NULL;
-    /* End to free avl tree with size of 100000 */
-
-
-    ////////////////////////////////////////////////////////////// Increase size 2
-
-    my_tree = create_avl(&compare_int, NULL, sizeof(int));
-
-    /* Insert 8000000 ints into the avl and count the time */
-    begin = clock();
-
-    for (int i = 0; i < 8000000; ++i) {
-        err = avl_insert(my_tree, toptr(i));
-
-        if (SCL_OK != err) {
-            scl_error_message(err);
-        }
-    }
-
-    end = clock();
-
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Inserting 8000000 ints into avl: %lf sec\n", exec_time);
-    /* End to insert 8000000 ints into the avl */
-
-    /* Delete 8000000 roots from avl and count the time */
-    begin = clock();
-
-    for (int i = 0; i < 8000000; ++i) {
-        err = avl_delete(my_tree, my_tree->root->data);
-
-        if (SCL_OK != err) {
-            scl_error_message(err);
-        }
-    }
-
-    end = clock();
-
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Deleting 8000000 ints roots from avl: %lf sec\n", exec_time);
-    /* End to delete 8000000 roots from avl */
-
-    /* Free avl tree with size of 8000000 and count time */
-    for (int i = 0; i < 8000000; ++i) {
-        avl_insert(my_tree, toptr(i));
-    }
-
-    begin = clock();
-
-    free_avl(my_tree);
-    
-    end = clock();
-    
-    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
-    printf("Freeing avl with 8000000 ints: %lf sec\n", exec_time);
-    
-    my_tree = NULL;
-    /* End to free avl tree with size of 8000000 */
-    
-    /* Close output file */
     fclose(fout);
+
+    free_avl(data_base);
 
     return 0;
 }
--- examples/avl_tree/example3.c
+++ examples/avl_tree/example3.c
@@ -1,4 +1,5 @@
 #include <scl_datastruc.h>
+#include <time.h>
 
 int main(void) {
     FILE *fout = NULL;
@@ -7,8 +8,179 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
-    printf("SUCCESS\n");
+    printf("In this example we will try to do different operations on AVL Tree and measure the time\n");
 
+    /* Init the working strucutres */
+    double exec_time = 0.0;
+
+    scl_error_t err = SCL_OK;
+
+    avl_tree_t *my_tree = create_avl(&compare_int, NULL, sizeof(int));
+    /* End to init the working structures */
+
+    /* Insert 100 ints into the avl and count the time */
+    clock_t begin = clock();
+
+    for (int i = 0; i < 100; ++i) {
+        err = avl_insert(my_tree, toptr(i));
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    clock_t end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 100 ints into avl: %lf sec\n", exec_time);
+    /* End to insert 100 ints into the avl */
+
+    /* Delete 100 roots from avl and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100; ++i) {
+        err = avl_delete(my_tree, my_tree->root->data);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 100 ints roots from avl: %lf sec\n", exec_time);
+    /* End to delete 100 roots from avl */
+
+    /* Free avl tree with size of 100 and count time */
+    for (int i = 0; i < 100; ++i) {
+        avl_insert(my_tree, toptr(i));
+    }
+
+    begin = clock();
+
+    free_avl(my_tree);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing avl with 100 ints: %lf sec\n\n", exec_time);
+    
+    my_tree = NULL;
+    /* End to free avl tree with size of 100 */
+
+    ////////////////////////////////////////////////////////////// Increase size 1
+
+    my_tree = create_avl(&compare_int, NULL, sizeof(int));
+
+    /* Insert 100000 ints into the avl and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100000; ++i) {
+        err = avl_insert(my_tree, toptr(i));
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 100000 ints into avl: %lf sec\n", exec_time);
+    /* End to insert 100000 ints into the avl */
+
+    /* Delete 100000 roots from avl and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100000; ++i) {
+        err = avl_delete(my_tree, my_tree->root->data);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 100000 ints roots from avl: %lf sec\n", exec_time);
+    /* End to delete 100000 roots from avl */
+
+    /* Free avl tree with size of 100000 and count time */
+    for (int i = 0; i < 100000; ++i) {
+        avl_insert(my_tree, toptr(i));
+    }
+
+    begin = clock();
+
+    free_avl(my_tree);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing avl with 100000 ints: %lf sec\n\n", exec_time);
+    
+    my_tree = NULL;
+    /* End to free avl tree with size of 100000 */
+
+
+    ////////////////////////////////////////////////////////////// Increase size 2
+
+    my_tree = create_avl(&compare_int, NULL, sizeof(int));
+
+    /* Insert 8000000 ints into the avl and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 8000000; ++i) {
+        err = avl_insert(my_tree, toptr(i));
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 8000000 ints into avl: %lf sec\n", exec_time);
+    /* End to insert 8000000 ints into the avl */
+
+    /* Delete 8000000 roots from avl and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 8000000; ++i) {
+        err = avl_delete(my_tree, my_tree->root->data);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 8000000 ints roots from avl: %lf sec\n", exec_time);
+    /* End to delete 8000000 roots from avl */
+
+    /* Free avl tree with size of 8000000 and count time */
+    for (int i = 0; i < 8000000; ++i) {
+        avl_insert(my_tree, toptr(i));
+    }
+
+    begin = clock();
+
+    free_avl(my_tree);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing avl with 8000000 ints: %lf sec\n", exec_time);
+    
+    my_tree = NULL;
+    /* End to free avl tree with size of 8000000 */
+    
+    /* Close output file */
     fclose(fout);
 
     return 0;
