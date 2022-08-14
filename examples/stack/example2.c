e06dbf6f0ee6e5af284bc4d04dafaf2b4d004dad
Mihai Negru <determinant289@gmail.com>
2022-08-14 13:26:17 +0300

    Finished stack examples

--- examples/stack/example2.c
+++ examples/stack/example2.c
@@ -1,4 +1,13 @@
 #include <scl_datastruc.h>
+#include <time.h>
+
+void free_st(void * const st) {
+    if (NULL != st) {
+        stack_t ** const t_st = st;
+
+        free_stack(*t_st);
+    }
+}
 
 int main(void) {
     FILE *fout = NULL;
@@ -7,7 +16,67 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
-    printf("SUCCESS\n");
+    stack_t *sts = create_stack(&free_st, sizeof(sts));
+
+    if (NULL != sts) {
+        srand(time(NULL));
+
+        scl_error_t err = SCL_OK;
+
+        for (int i = 0; i < 3; ++i) {
+            stack_t *st = create_stack(NULL, sizeof(int));
+
+            for (int j = 0; j < 10; ++j) {
+                err = stack_push(st, ltoptr(int, rand() % 100 + 10));
+
+                if (SCL_OK != err) {
+                    scl_error_message(err);
+                }
+            }
+
+            err = stack_push(sts, toptr(st));
+
+            if (SCL_OK != err) {
+                scl_error_message(err);
+            }
+        }
+
+        int stacks_num = 0;
+
+        while (!is_stack_empty(sts)) {
+            printf("Queue number %d:\n", stacks_num++);
+
+            stack_t * const * top = stack_top(sts);
+
+            while (!is_stack_empty(*top)) {
+                const int *top_elem = stack_top(*top);
+
+                printf("%d ", *top_elem);
+
+                err = stack_pop(*top);
+
+                if (SCL_OK != err) {
+                    scl_error_message(err);
+                }
+            }
+
+            printf("\n\n");
+
+            // Popping the stack has to be the last operation
+            // because popping will remove stack from memory
+            // If you do not want pop to remove stack from heap
+            // You shall not specify a free function into the
+            // craettion of stack, and then the free will be
+            // done manually by you
+            err = stack_pop(sts);
+
+            if (SCL_OK != err) {
+                scl_error_message(err);
+            }
+        }
+
+        free_stack(sts);
+    }
 
     fclose(fout);
 
--- examples/stack/example3.c
+++ examples/stack/example3.c
@@ -1,4 +1,5 @@
 #include <scl_datastruc.h>
+#include <time.h>
 
 int main(void) {
     FILE *fout = NULL;
@@ -7,8 +8,179 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
-    printf("SUCCESS\n");
+    printf("In this example we will try to do different operations on Stacks and measure the time\n\n");
 
+    /* Init the working strucutres */
+    double exec_time = 0.0;
+
+    scl_error_t err = SCL_OK;
+
+    stack_t *st = create_stack(NULL, sizeof(int));
+    /* End to init the working structures */
+
+    /* Insert 100 ints into the stack and count the time */
+    clock_t begin = clock();
+
+    for (int i = 0; i < 100; ++i) {
+        err = stack_push(st, toptr(i));
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    clock_t end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 100 ints into stack: %lf sec\n", exec_time);
+    /* End to insert 100 ints into the stack */
+
+    /* Delete 100 tops from stack and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100; ++i) {
+        err = stack_pop(st);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 100 ints tops from stack: %lf sec\n", exec_time);
+    /* End to delete 100 tops from stack */
+
+    /* Free stack with size of 100 and count time */
+    for (int i = 0; i < 100; ++i) {
+        stack_push(st, toptr(i));
+    }
+
+    begin = clock();
+
+    free_stack(st);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing stack with 100 ints: %lf sec\n\n", exec_time);
+    
+    st = NULL;
+    /* End to free stack with size of 100 */
+
+    ////////////////////////////////////////////////////////////// Increase size 1
+
+    st = create_stack(NULL, sizeof(int));
+
+    /* Insert 100000 ints into the stack and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100000; ++i) {
+        err = stack_push(st, toptr(i));
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 100000 ints into stack: %lf sec\n", exec_time);
+    /* End to insert 100000 ints into the stack */
+
+    /* Delete 100000 tops from stack and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100000; ++i) {
+        err = stack_pop(st);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 100000 ints tops from stack: %lf sec\n", exec_time);
+    /* End to delete 100000 tops from stack */
+
+    /* Free stack with size of 100000 and count time */
+    for (int i = 0; i < 100000; ++i) {
+        stack_push(st, toptr(i));
+    }
+
+    begin = clock();
+
+    free_stack(st);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing stack with 100000 ints: %lf sec\n\n", exec_time);
+    
+    st = NULL;
+    /* End to free stack with size of 100000 */
+
+
+    ////////////////////////////////////////////////////////////// Increase size 2
+
+    st = create_stack(NULL, sizeof(int));
+
+    /* Insert 8000000 ints into the stack and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 8000000; ++i) {
+        err = stack_push(st, toptr(i));
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 8000000 ints into stack: %lf sec\n", exec_time);
+    /* End to insert 8000000 ints into the stack */
+
+    /* Delete 8000000 tops from stack and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 8000000; ++i) {
+        err = stack_pop(st);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 8000000 ints tops from stack: %lf sec\n", exec_time);
+    /* End to delete 8000000 tops from stack */
+
+    /* Free stack with size of 8000000 and count time */
+    for (int i = 0; i < 8000000; ++i) {
+        stack_push(st, toptr(i));
+    }
+
+    begin = clock();
+
+    free_stack(st);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing stack with 8000000 ints: %lf sec\n", exec_time);
+    
+    st = NULL;
+    /* End to free stack with size of 8000000 */
+    
+    /* Close output file */
     fclose(fout);
 
     return 0;
