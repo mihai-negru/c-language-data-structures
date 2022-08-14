4f36947a9eafc51fae2269f2b071d15c350db452
Mihai Negru <determinant289@gmail.com>
2022-08-14 15:36:37 +0300

    Finished priority queue examples

--- examples/priority_queue/example1.c
+++ examples/priority_queue/example1.c
@@ -1,5 +1,39 @@
 #include <scl_datastruc.h>
 
+#define MAX_ELEMENTS 100
+#define MAX_STRING 100
+
+int32_t compute_voc(const char * const str) {
+    int32_t voc_diff = 0;
+    
+    for (size_t iter = 0; str[iter] != 0; ++iter) {
+        if (('a' == str[iter]) || 
+            ('e' == str[iter]) ||
+            ('i' == str[iter]) ||
+            ('o' == str[iter]) ||
+            ('u' == str[iter]) ||
+            ('A' == str[iter]) ||
+            ('E' == str[iter]) ||
+            ('I' == str[iter]) ||
+            ('O' == str[iter]) ||
+            ('U' == str[iter])) {
+                ++voc_diff;
+        }
+    }
+
+    return voc_diff;
+}
+
+void capitalize(void * const data) {
+    if (NULL != data) {
+        char * const t_d = data;
+
+        if ((t_d[0] >= 97) && (t_d[0] <= 122)) {
+            t_d[0] -= 32;
+        } 
+    }
+}
+
 int main(void) {
     FILE *fout = NULL;
 
@@ -7,7 +41,103 @@ int main(void) {
         exit(EXIT_FAILURE);
     }
 
-    printf("SUCCESS\n");
+    FILE *fin = NULL;
+
+    if ((fin = freopen("example1.in", "r", stdin)) == NULL) {
+        fclose(fout);
+        exit(EXIT_FAILURE);
+    }
+
+    priority_queue_t *pq = create_priority_queue(MAX_ELEMENTS, &compare_int, &compare_string, NULL, NULL, sizeof(int), MAX_STRING);
+
+    int elem_num = 0; 
+    
+    int check = scanf("%d", &elem_num);
+
+    if (check > 1) {
+        exit(EXIT_FAILURE);
+    }
+
+    
+    scl_error_t err = SCL_OK;
+
+    for (int i = 0; i < elem_num; ++i) {
+        char read_data[MAX_STRING];
+
+        check = scanf("%s", read_data);
+
+        if (check > 1) {
+            exit(EXIT_FAILURE);
+        }    
+
+        err = pri_queue_push(pq, ltoptr(int32_t, compute_voc(read_data)), read_data);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    printf("Priority queue data:\n");
+    pri_queue_traverse(pq, &print_string);
+    printf("\n\n");
+
+    const char *top = pri_queue_top(pq);
+    
+    if (NULL != top) {
+        const int *voc = pri_queue_top_pri(pq);
+
+        printf("The top elements is \"%s\" and has %d vowels:\n\n", top, *voc);
+    }
+
+    printf("Let's remove the top element:\n");
+    err = pri_queue_pop(pq);
+
+    if (SCL_OK != err) {
+        scl_error_message(err);
+    }
+
+    pri_queue_traverse(pq, &print_string);
+    printf("\n\n");
+
+    printf("Let's change now the top element into \"!!!!HELLO!!!!\" and also to change it's priority\n");
+
+    size_t top_index = pri_find_data_index(pq, "communication");
+
+    err = change_node_data(pq, top_index, "!!!!HELLO!!!!");
+
+    if (SCL_OK != err) {
+        scl_error_message(err);
+    }
+
+    err = change_node_priority(pq, top_index, ltoptr(int32_t, compute_voc("!!!!HELLO!!!!")));
+
+    if (SCL_OK != err) {
+        scl_error_message(err);
+    }
+
+    pri_queue_traverse(pq, &print_string);
+    printf("\n\n");
+
+    printf("Let's pop 10 elems from priority queue:\n");
+    for (size_t i = 0; i < 10; ++i) {
+        err = pri_queue_pop(pq);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    pri_queue_traverse(pq, &print_string);
+    printf("\n\n");
+
+    printf("For the remaining strings let's capitalize them\n");
+    pri_queue_traverse(pq, &capitalize);
+    pri_queue_traverse(pq, &print_string);
+    printf("\n");
+
+    fclose(fin);
+
+    free_priority_queue(pq);
 
     fclose(fout);
 
--- examples/priority_queue/example2.c
+++ examples/priority_queue/example2.c
@@ -1,14 +1,186 @@
 #include <scl_datastruc.h>
+#include <time.h>
 
 int main(void) {
     FILE *fout = NULL;
 
-    if ((fout = freopen("out_2.txt", "w", stdout)) == NULL) {
+    if ((fout = freopen("out_3.txt", "w", stdout)) == NULL) {
         exit(EXIT_FAILURE);
     }
 
-    printf("SUCCESS\n");
+    printf("In this example we will try to do different operations on Priority Queues and measure the time\n\n");
 
+    /* Init the working strucutres */
+    double exec_time = 0.0;
+
+    scl_error_t err = SCL_OK;
+
+    priority_queue_t *pq = create_priority_queue(100, &compare_int, NULL, NULL, NULL, sizeof(int), 0);
+    /* End to init the working structures */
+
+    /* Insert 100 ints into the pri queue and count the time */
+    clock_t begin = clock();
+
+    for (int i = 0; i < 100; ++i) {
+        err = pri_queue_push(pq, toptr(i), NULL);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    clock_t end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 100 ints into pri queue: %lf sec\n", exec_time);
+    /* End to insert 100 ints into the pri queue */
+
+    /* Delete 100 fronts from pri queue and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100; ++i) {
+        err = pri_queue_pop(pq);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 100 ints fronts from pri queue: %lf sec\n", exec_time);
+    /* End to delete 100 fronts from pri queue */
+
+    /* Free pri queue with size of 100 and count time */
+    for (int i = 0; i < 100; ++i) {
+        pri_queue_push(pq, toptr(i), NULL);
+    }
+
+    begin = clock();
+
+    free_priority_queue(pq);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing pri queue with 100 ints: %lf sec\n\n", exec_time);
+    
+    pq = NULL;
+    /* End to free pri queue with size of 100 */
+
+    ////////////////////////////////////////////////////////////// Increase size 1
+
+    pq = create_priority_queue(100000, &compare_int, NULL, NULL, NULL, sizeof(int), 0);
+
+    /* Insert 100000 ints into the pri queue and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100000; ++i) {
+        err = pri_queue_push(pq, toptr(i), NULL);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 100000 ints into pri queue: %lf sec\n", exec_time);
+    /* End to insert 100000 ints into the pri queue */
+
+    /* Delete 100000 fronts from pri queue and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 100000; ++i) {
+        err = pri_queue_pop(pq);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 100000 ints fronts from pri queue: %lf sec\n", exec_time);
+    /* End to delete 100000 fronts from pri queue */
+
+    /* Free pri queue with size of 100000 and count time */
+    for (int i = 0; i < 100000; ++i) {
+        pri_queue_push(pq, toptr(i), NULL);
+    }
+
+    begin = clock();
+
+    free_priority_queue(pq);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing pri queue with 100000 ints: %lf sec\n\n", exec_time);
+    
+    pq = NULL;
+    /* End to free pri queue with size of 100000 */
+
+
+    ////////////////////////////////////////////////////////////// Increase size 2
+
+    pq = create_priority_queue(8000000, &compare_int, NULL, NULL, NULL, sizeof(int), 0);
+
+    /* Insert 8000000 ints into the pri queue and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 8000000; ++i) {
+        err = pri_queue_push(pq, toptr(i), NULL);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Inserting 8000000 ints into pri queue: %lf sec\n", exec_time);
+    /* End to insert 8000000 ints into the pri queue */
+
+    /* Delete 8000000 fronts from pri queue and count the time */
+    begin = clock();
+
+    for (int i = 0; i < 8000000; ++i) {
+        err = pri_queue_pop(pq);
+
+        if (SCL_OK != err) {
+            scl_error_message(err);
+        }
+    }
+
+    end = clock();
+
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Deleting 8000000 ints fronts from pri queue: %lf sec\n", exec_time);
+    /* End to delete 8000000 fronts from pri queue */
+
+    /* Free pri queue with size of 8000000 and count time */
+    for (int i = 0; i < 8000000; ++i) {
+        pri_queue_push(pq, toptr(i), NULL);
+    }
+
+    begin = clock();
+
+    free_priority_queue(pq);
+    
+    end = clock();
+    
+    exec_time = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Freeing pri queue with 8000000 ints: %lf sec\n", exec_time);
+    
+    pq = NULL;
+    /* End to free pri queue with size of 8000000 */
+    
+    /* Close output file */
     fclose(fout);
 
     return 0;
