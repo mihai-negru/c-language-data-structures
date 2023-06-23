6920579f62b00e8bb4628e6def2f175f37eb0bc0
mihai-negru <determinant289@gmail.com>
2023-06-23 20:35:20 +0300

    Finished the configuration file for the structures

--- macros_data_structs/src/m_config.h
+++ macros_data_structs/src/m_config.h
@@ -33,21 +33,23 @@
 
 typedef enum mbool_s { mfalse = 0, mtrue = 1 } mbool_t;
 
+/**
+ * @brief Enum for error handling.
+ */
 typedef enum merr_s {
-  M_OK,
-
-  M_MUST_BE_NULL,
-  M_MALLOC_FAILED,
-  M_REALLOC_FAILED,
-  M_FREE_NULL,
-  M_NULL_INPUT,
-  M_NOT_FOUND,
-  M_POP_FROM_EMPTY,
-  M_IDX_OVERFLOW,
-  M_NULL_ACTION,
-  M_EMPTY_STRUCTURE,
-  M_INVALID_INPUT,
-  M_UNDEFINED_BEHAVIOUR
+  M_OK = 0,
+  M_MUST_BE_NULL = -1,
+  M_MALLOC_FAILED = -2,
+  M_REALLOC_FAILED = -3,
+  M_FREE_NULL = -4,
+  M_NULL_INPUT = -5,
+  M_NOT_FOUND = -6,
+  M_POP_FROM_EMPTY = -7,
+  M_IDX_OVERFLOW = -8,
+  M_NULL_ACTION = -9,
+  M_EMPTY_STRUCTURE = -10,
+  M_INVALID_INPUT = -11,
+  M_UNDEFINED_BEHAVIOUR = -12
 } merr_t;
 
 #define CMP_FUNC(ID, T)                                                        \
@@ -61,5 +63,53 @@ typedef enum merr_s {
   typedef T2 (*ID1##_to_##ID2##_map_func)(const T1 *const);
 
 #define ACTION_FUNC(ID, T) typedef void (*ID##_action_func)(const T *const);
+
+#define MERROR(error)                                                          \
+  do {                                                                         \
+    switch (error) {                                                           \
+    case M_OK:                                                                 \
+      fprintf(stderr, "[OK] No errors generated.\n");                          \
+      break;                                                                   \
+    case M_MUST_BE_NULL:                                                       \
+      fprintf(stderr, "[ERROR] The input must be `NULL`.\n");                  \
+      break;                                                                   \
+    case M_MALLOC_FAILED:                                                      \
+      fprintf(stderr, "[ERROR] Failed to malloc heap memory for object.\n");   \
+      break;                                                                   \
+    case M_REALLOC_FAILED:                                                     \
+      fprintf(stderr, "[ERROR] Failed to realloc a memory zone.\n");           \
+      break;                                                                   \
+    case M_FREE_NULL:                                                          \
+      fprintf(stderr, "[ERROR] Freeing a `NULL` object.\n");                   \
+      break;                                                                   \
+    case M_NULL_INPUT:                                                         \
+      fprintf(stderr, "[ERROR] The input is `NULL`.\n");                       \
+      break;                                                                   \
+    case M_NOT_FOUND:                                                          \
+      fprintf(stderr, "[ERROR] The data was not found inside the object.\n");  \
+      break;                                                                   \
+    case M_POP_FROM_EMPTY:                                                     \
+      fprintf(stderr, "[ERROR] Poping from an empty object.\n");               \
+      break;                                                                   \
+    case M_IDX_OVERFLOW:                                                       \
+      fprintf(stderr, "[ERROR] Index is to large for the object.\n");          \
+      break;                                                                   \
+    case M_NULL_ACTION:                                                        \
+      fprintf(stderr, "[ERROR] Action function is `NULL`.\n");                 \
+      break;                                                                   \
+    case M_EMPTY_STRUCTURE:                                                    \
+      fprintf(stderr, "[ERROR] Fetching data from an empty structure.\n");     \
+      break;                                                                   \
+    case M_INVALID_INPUT:                                                      \
+      fprintf(stderr, "[ERROR] Input is invalid for the method type.\n");      \
+      break;                                                                   \
+    case M_UNDEFINED_BEHAVIOUR:                                                \
+      fprintf(stderr, "[ERROR] The method enters in an undefined state "       \
+                      "becasue of the input.\n");                              \
+      break;                                                                   \
+    default:                                                                   \
+      fprintf(stderr, "[ERROR] The error code is not known.\n");               \
+    }                                                                          \
+  } while (0)
 
 #endif /* MACROS_GENERICS_CONFIG_H_ */
